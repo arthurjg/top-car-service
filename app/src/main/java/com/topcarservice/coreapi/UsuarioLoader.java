061ea049b80a1ca159032fafee506669c29578cd
DESKTOP-EM7D74H\arthur <arthur.ttco@hotmail.com>
2026-05-06 16:46:35 -0300

    implementado rotina para criação de usuário padrão.

---
+++ app/src/main/java/com/topcarservice/coreapi/UsuarioLoader.java
@@ -0,0 +1,52 @@
+package com.topcarservice.coreapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.topcarservice.coreapi.domain.Usuario;
+import com.topcarservice.coreapi.repository.UsuarioRepository;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class UsuarioLoader {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@PostConstruct
+	public void load() {
+		
+		log.info("Carregando usuário padrão...");
+		
+		String username = "fgarage";
+		
+		Usuario usuario = repository.findByLogin(username);
+		
+		if (usuario != null) {
+			log.info("usuário padrão existente...");
+			return;
+		}
+		
+		String senha = "123456";
+		
+		var encriptador = new BCryptPasswordEncoder();
+		
+		String senhaCriptografada = encriptador.encode(senha);
+		
+		usuario = Usuario.builder()
+				.nome("Flavio")
+				.login(username)
+				.senha(senhaCriptografada)
+				.build();		
+		
+		repository.save(usuario);
+		
+		log.info("usuário padrão criado...");
+		
+	}
+
+}
